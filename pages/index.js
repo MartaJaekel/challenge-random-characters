5c6e68b7fb1dbd10d7dcd3ce1ee05eb787e6f554
Marta Jäkel <martajaekel@gmail.com>
2023-11-30 15:15:46 +0100

    complete challenge random characters

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,33 @@
+import useSWR from "swr";
+import { useRouter } from "next/router";
+const fetcher = (...args) => fetch(...args).then((res) => res.json());
+
 export default function HomePage() {
-  return <h1>Hello from Next.js!</h1>;
+  const router = useRouter();
+  const { id } = router.query;
+
+  const { data: character, error } = useSWR(`/api/random-character`, fetcher);
+  if (error) {
+    return <h1>Error loading character data</h1>;
+  }
+
+  if (!character) {
+    return <h1>Loading...</h1>;
+  }
+
+
+
+
+
+
+
+  return (
+    <>
+      <h1>Character Details</h1>
+       <p>Name: {character.name}</p>
+     <p>Twitter: {character.twitterName}</p>
+      <p>Geohash: {character.geohash}</p>
+
+      </>
+  )
 }
